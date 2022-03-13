2240c4189d16b0ad71d546ca753338c240e888b5
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-13 17:43:42 +0300

    user login logic

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,7 @@
-from email import message
-
 from app.email import mail_message
 from .. import db
 from . import auth
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, request, flash
 from flask_login import login_user, logout_user, login_required
 from app.auth.forms import RegistrationForm, LoginForm
 from app.models import User
@@ -27,6 +25,15 @@ def register():
 @auth.route('/login')
 def login():
     login_form = LoginForm()
+
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('invalid username or password')
+
     return render_template('auth/login.html', login_form = login_form)
 
 @auth.route('/logout')
