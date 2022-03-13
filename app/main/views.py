3086ffb2d50038902d3a8fa0d067addeeab10b77
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-13 23:23:02 +0300

    import models

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,13 @@
-from flask import render_template, redirect, url_for
+import imp
+from flask import render_template, redirect, url_for, flash, abort, request
 from . import main
+from .forms import UpdateProfile, CategoryForm, CommentForm
+from flask_login import login_required, current_user
+from ..models import User, Post, Category, Comment
 
 @main.route('/')
 def index():
+
+    
 
     return render_template('index.html')
