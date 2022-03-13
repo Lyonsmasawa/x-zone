b61a0e49e5f972247ecf64901715702fe227e6b8
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-13 16:30:52 +0300

    create login and registration views

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,17 +1,24 @@
 from crypt import methods
+from inspect import _void
 from .. import db
 from . import auth
 from flask import render_template
+from flask_login import login_user, logout_user, login_required
 from app.auth.forms import RegistrationForm, LoginForm
 
-@auth.route('/register', methods = ["GET", "POST"])
+@auth.route('/register')
 def register():
     form = RegistrationForm()
 
-    return render_template('register.html', register_form = form)
+    return render_template('auth/register.html', register_form = form)
 
-@auth.route('/login', methods = ["GET", "POST"])
+@auth.route('/login')
 def login():
-    form = LoginForm()
+    login_form = LoginForm()
+    return render_template('auth/login.html', login_form = login_form)
 
-    return render_template('register.html', login_form = form)
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return
