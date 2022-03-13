bbc24a43f6f71cfb695771d11131fd2967834af6
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-13 17:12:08 +0300

    user registration logic

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,14 +1,20 @@
-from crypt import methods
-from inspect import _void
 from .. import db
 from . import auth
 from flask import render_template
 from flask_login import login_user, logout_user, login_required
 from app.auth.forms import RegistrationForm, LoginForm
+from app.models import User
 
 @auth.route('/register')
 def register():
     form = RegistrationForm()
+    
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+    
 
     return render_template('auth/register.html', register_form = form)
 
