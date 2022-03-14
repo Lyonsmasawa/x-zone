b698f2370dbe27b93aa2d38e7d3174fa11cef397
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-15 00:04:58 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -14,8 +14,7 @@ class Config:
     SIMPLEMDE_USE_CDN = True
 
 class ProdConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace('postgres://', 'postgresql://')
 
 class TestConfig(Config):
     pass
