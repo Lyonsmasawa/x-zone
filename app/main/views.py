56de13bbc8b142ec94fe9454665ce1291e0141a2
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-14 05:49:22 +0300

    add link to profile

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,6 @@
+from crypt import methods
 import imp
+import profile
 from flask import render_template, redirect, url_for, flash, abort, request
 from . import main
 from .forms import UpdateProfile, CategoryForm, CommentForm
@@ -11,3 +13,9 @@ def index():
     posts =  Post.query.order_by(Post.posted.desc()).all()
 
     return render_template('index.html', posts = posts)
+
+@main.route('/profile/<username>', methods=['GET', 'POST'])
+def profile(username):
+
+
+    return render_template('profile/profile.html')
