1fe9cba4d8466196838243669e758f5499735ba2
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-03-14 06:26:40 +0300

    add update profile view

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,9 @@
-from crypt import methods
-import imp
-import profile
 from flask import render_template, redirect, url_for, flash, abort, request
 from . import main
 from .forms import UpdateProfile, CategoryForm, CommentForm
 from flask_login import login_required, current_user
 from ..models import User, Post, Category, Comment
+from app import db
 
 @main.route('/')
 def index():
@@ -24,3 +22,22 @@ def profile(username):
     posts = Post.get_user_posts(user.id).order_by(Post.posted.desc()).all()
 
     return render_template('profile/profile.html', user = user, posts = posts)
+
+@main.route('/profile/<username>/update', method = ['GET', 'POST'])
+def update_profile(username):
+
+    user = User.query.filter_by(username=username).first()
+
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', username=user.username))
+    
+    return render_template('profile/update_profile.html')
